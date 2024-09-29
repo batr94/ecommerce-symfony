972a55a311e2c7fbdda8591a00d4aa21c729e2d6
batr94 <www.bat.94@gmail.com>
2024-09-29 22:03:08 +0500

    add RegisterUserDTO as registration action parameter

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,15 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use App\DTO\RegisterUserDTO;
 
 #[Route('/security', name: 'app_security_')]
 class SecurityController extends AbstractController
 {
     #[Route('/registration', methods: ['POST'], name: 'registration')]
-    public function registration(): JsonResponse
-    {
+    public function registration(
+        #[MapRequestPayload] RegisterUserDTO $registerUserDTO
+    ): JsonResponse {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => 'Hello',
             'path' => 'src/Controller/SecurityController.php',
         ]);
     }
