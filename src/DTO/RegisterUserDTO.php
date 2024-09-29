9d753be1b8f39ed92deb1f03cc668b907c5aff65
batr94 <www.bat.94@gmail.com>
2024-09-29 22:01:57 +0500

    changed password strength and confirmPassword's message in RegisterUserDTO

--- src/DTO/RegisterUserDTO.php
+++ src/DTO/RegisterUserDTO.php
@@ -11,9 +11,9 @@ final readonly class RegisterUserDTO
   public function __construct(
     #[Assert\Email(message: 'Invalid email')]
     public string $emailAddress,
-    #[Assert\PasswordStrength]
+    #[Assert\PasswordStrength(minScore: Assert\PasswordStrength::STRENGTH_WEAK)]
     public string $password,
-    #[Assert\EqualTo(propertyPath: 'password')]
+    #[Assert\EqualTo(propertyPath: 'password', message: 'The passwords do not match')]
     public string $comparePassword
   ) {
   }
