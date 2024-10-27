4c2edd9af47db424a47702c4b325fb1b0fb65d73
batr94 <www.bat.94@gmail.com>
2024-10-27 12:10:00 +0500

    add emailAddress field unique validation to RegisterUserDTO

--- src/DTO/RegisterUserDTO.php
+++ src/DTO/RegisterUserDTO.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace App\DTO;
 
+use App\Entity\SiteUser;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
+#[UniqueEntity(
+  fields: ['emailAddress' => 'email_address'],
+  entityClass: SiteUser::class,
+)]
 final readonly class RegisterUserDTO
 {
+  /**
+   * TODO: Добавить проверку на уникальность email и номера телефона
+   * TODO: Добавить проверку на правильность номера телефона
+   */
   public function __construct(
     #[Assert\Email(message: 'Invalid email')]
     public string $emailAddress,
